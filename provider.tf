2cca4ae084627a27e4dfbc155d5ed1fc15c7575b
Yhonny27 <99502467+Yhonny27@users.noreply.github.com>
2022-03-25 08:42:46 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,3 +4,17 @@ provider "google" {
   region       = "us-central1"
   zone         = "us-central1-c"
 }
+provider "kubernetes" {
+
+    config_path    = "~/.kube/config"
+    config_context = "google_container_cluster.private-cluster.name"
+    host           = data.google_container_cluster.cluster.endpoint
+    insecure       = true
+
+}
+data "google_container_cluster" "cluster" {
+
+    name     = "private-cluster"
+    location = "us-central1"
+
+}
