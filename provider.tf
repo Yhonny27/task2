9a6ff8815b53d9a6689669b10a61334783b27c07
Yhonny27 <y.ca@outlook.com>
2022-03-25 18:44:44 -0600

    subir provider y lock

--- provider.tf
+++ provider.tf
@@ -4,16 +4,22 @@ provider "google" {
   region       = "us-central1"
   zone         = "us-central1-c"
 }
-data "google_client_config" "provider" {}
-data "google_container_cluster" "my_cluster" {
-  name     = "private-cluster"
-  location = "us-central1"
-}
+# data "google_client_config" "provider" {}
+# data "google_container_cluster" "my_cluster" {
+#   name     = "private-cluster"
+#   location = "us-central1"
+# }
 
+#provider "kubernetes" {
+#  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+#  token = data.google_client_config.provider.access_token
+#  cluster_ca_certificate = base64decode(
+#    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+#  )
+#}
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
-  )
+  host = "https://${google_container_cluster.private-cluster.endpoint}"
+  client_certificate     = google_container_cluster.private-cluster.master_auth.0.client_certificate 
+  client_key             = google_container_cluster.private-cluster.master_auth.0.client_key
+  cluster_ca_certificate = base64decode(google_container_cluster.private-cluster.master_auth.0.cluster_ca_certificate)
 }
